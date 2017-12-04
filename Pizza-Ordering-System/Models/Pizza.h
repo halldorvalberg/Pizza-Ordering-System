334cd1dd49d3c6481fc5a2dcb4be4b6648a06741
halldorvalberg <34029264+halldorvalberg@users.noreply.github.com>
2017-12-04 18:11:40 +0000

    fix

--- Pizza-Ordering-System/Models/Pizza.h
+++ Pizza-Ordering-System/Models/Pizza.h
@@ -10,11 +10,8 @@ class Pizza
 {
     public:
         Pizza();
-<<<<<<< HEAD
         Pizza(string name, int price, char size);
-=======
         Pizza(string name, vector<Toppings> toppings, int size, int price);
->>>>>>> a0bd809764329400139765c761b9eb3f268370f6
         string getpizzaname();
         int getpizzasize();
         int getpizzaprice();
